8521394f245a295ffbb244dc2113becaeaa77b88
Felipe Schoffen Tressino <fs.tressino@gmail.com>
2025-12-18 21:00:45 -0300

    feat: add validation helper for dates input

---
+++ schoffen.CodingTracker/schoffen.CodingTracker/Validator/ValidationHelper.cs
@@ -0,0 +1,16 @@
+﻿using System.Globalization;
+
+namespace schoffen.CodingTracker.Validator;
+
+public class ValidationHelper
+{
+    public static bool IsDateValid(string date)
+    {
+        return DateTime.TryParseExact(
+            date, 
+            "dd/MM/yyyy HH:mm", 
+            CultureInfo.InvariantCulture, 
+            DateTimeStyles.None,
+            out _);
+    }
+}
